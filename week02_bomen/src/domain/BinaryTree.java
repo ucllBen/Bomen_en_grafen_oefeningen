e3df02877a7c34ddc9a89e19f2f4a42a2040534c
ucllBen <ben.lamot@student.ucll.be>
2022-02-28 12:51:17 +0100

    week 2 met voorbeeldoplossing

--- week02_bomen/src/domain/BinaryTree.java
+++ week02_bomen/src/domain/BinaryTree.java
@@ -1,26 +1,94 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class BinaryTree<E> {
 	private E data;
 	private BinaryTree<E> leftTree, rightTree;
-	
-	public BinaryTree(E data){
-		this(data,null,null);
+
+	public BinaryTree(E data) {
+		this(data, null, null);
 	}
-	
-	public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree){
+
+	public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree) {
 		if (data == null) {
 			throw new IllegalArgumentException();
 		}
-		this.data= data;
-		this.leftTree= leftTree;
-		this.rightTree= rightTree;
+		this.data = data;
+		this.leftTree = leftTree;
+		this.rightTree = rightTree;
 	}
-	
-	public void printPreorder(){
-			System.out.print(this.data + " ");
-			if (this.leftTree != null) this.leftTree.printPreorder();
-			if (this.rightTree != null) this.rightTree.printPreorder();
+
+	public void printPreorder() {
+		System.out.print(this.data + " ");
+		if (this.leftTree != null) this.leftTree.printPreorder();
+		if (this.rightTree != null) this.rightTree.printPreorder();
+	}
+
+	public void printInorder() {
+
+		if (this.leftTree != null) this.leftTree.printInorder();
+		System.out.print(this.data + " ");
+		if (this.rightTree != null) this.rightTree.printInorder();
+
+	}
+
+	public void printPostorder() {
+		if (this.leftTree != null) this.leftTree.printPostorder();
+		if (this.rightTree != null) this.rightTree.printPostorder();
+		System.out.print(this.data + " ");
+	}
+
+	public int countNodes() {
+		return 1 + (this.leftTree == null ? 0 : this.leftTree.countNodes()) + (this.rightTree == null ? 0 : this.rightTree.countNodes());
+
+
+	}
+
+	public int getDepth() {
+		return 1 + Math.max((this.leftTree == null ? 0 : this.leftTree.getDepth()),
+				(this.rightTree == null ? 0 : this.rightTree.getDepth()));
+	}
+
+	public boolean isLeaf() {
+		return this.leftTree == null && this.rightTree == null;
+	}
+
+	public int countLeaves() {
+		if (this.isLeaf()) {
+			return 1;
+		} else {
+			return (this.leftTree == null ? 0 : this.leftTree.countLeaves())
+					+ (this.rightTree == null ? 0 : this.rightTree.countLeaves());
+		}
+	}
+
+	public ArrayList<E> getDataLeaves() {
+		ArrayList<E> res = new ArrayList<>();
+		if (this.isLeaf()) {
+			res.add(this.data);
+		} else {
+			res = (this.leftTree == null ? new ArrayList<>() : this.leftTree.getDataLeaves());
+			ArrayList<E> rightLeaves =
+					(this.rightTree == null ? new ArrayList<>() : this.rightTree.getDataLeaves());
+			res.addAll(rightLeaves);
+		}
+		return res;
+
+
+	}
+
+	public boolean contains(E s) {
+		if (s == null) {
+			return false;
+		}
+		if (s.equals(this.data)) {
+			return true;
+		} else {
+			return (this.leftTree == null ? false : this.leftTree.contains(s)) ||
+					(this.rightTree == null ? false : this.rightTree.contains(s));
+		}
+
 	}
 
 }
--- week02_bomen/src/ui/BinaryTreeDriver.java
+++ week02_bomen/src/ui/BinaryTreeDriver.java
@@ -5,21 +5,30 @@ import domain.BinaryTree;
 public class BinaryTreeDriver {
 
 	public static void main(String[] args) {
-		BinaryTree<String> nodeD = new BinaryTree<>("D");
-		BinaryTree<String> nodeF = new BinaryTree<>("F");
-		BinaryTree<String> nodeB = new BinaryTree<>("B");
+		BinaryTree<String> nodeC = new BinaryTree<>("C");
+		BinaryTree<String> nodeE = new BinaryTree<>("E");
 		BinaryTree<String> nodeH = new BinaryTree<>("H");
-		
-		// knoop A heeft links D en rechts F
-		BinaryTree<String> nodeA = new BinaryTree<>("A",nodeD, nodeF);
-		// knoop E heeft links H
-		BinaryTree<String> nodeE = new BinaryTree<>("E",nodeH,null);
-		// knoop G heeft links E en rechts B
-		BinaryTree<String> nodeG = new BinaryTree<>("G",nodeE, nodeB);
-		
-		// boom heeft root C en heeft links A en rechts G
-		BinaryTree<String> boom = new BinaryTree<>("C",nodeA, nodeG);
+		BinaryTree<String> nodeA = new BinaryTree<>("A");
+
+		BinaryTree<String> nodeD = new BinaryTree<>("D",nodeC,nodeE);
+		BinaryTree<String> nodeI = new BinaryTree<>("I",nodeH,null);
+
+
+
+		BinaryTree<String> nodeB = new BinaryTree<>("B",nodeA,nodeD);
+		BinaryTree<String> nodeG = new BinaryTree<>("G",null,nodeI);
+
+
+		BinaryTree<String> boom = new BinaryTree<>("F",nodeB,nodeG);
+
 		boom.printPreorder();
+		System.out.println();
+		boom.printInorder();
+		System.out.println();
+
+		boom.printPostorder();
+
+
 	}
 
 }
